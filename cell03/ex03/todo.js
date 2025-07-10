7a37529690818f42c3dd4756e4dac95f593f542d
Sérgio Curto <sergio.miguel.curto@gmail.com>
2025-07-10 14:41:25 +0100

    update

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,13 +1,13 @@
-function setCookie(name, value, days) {
+function setCookie(name, value, days) { /* Define a função para definir um cookie */
   var expires = '';
-  if (days) {
+  if (days) { /* Define o tempo de expiração do cookie */
     var date = new Date();
     date.setTime(date.getTime() + (days*24*60*60*1000));
     expires = '; expires=' + date.toUTCString();
   }
   document.cookie = name + '=' + encodeURIComponent(value) + expires + '; path=/';
 }
-function getCookie(name) {
+function getCookie(name) { /* Define a função para obter o valor de um cookie */
   var nameEQ = name + '=';
   var ca = document.cookie.split(';');
   for(var i=0;i < ca.length;i++) {
@@ -17,39 +17,39 @@ function getCookie(name) {
   }
   return null;
 }
-function saveTodos() {
+function saveTodos() { /* Salva a lista de TO DOs em um cookie */
   var todos = [];
   document.querySelectorAll('#ft_list .todo').forEach(function(el) {
     todos.push(el.textContent);
   });
   setCookie('todos', JSON.stringify(todos), 365);
 }
-function loadTodos() {
+function loadTodos() { /* Carrega a lista de TO DOs do cookie */
   var data = getCookie('todos');
   if (data) {
     try {
       var todos = JSON.parse(data);
-      todos.reverse().forEach(function(txt) {
+      todos.reverse().forEach(function(txt) { /* Adiciona cada TO DO à lista */
         addTodo(txt, false);
       });
     } catch(e) {}
   }
 }
-function addTodo(text, save=true) {
+function addTodo(text, save=true) { /* Adiciona um novo TO DO à lista */
   var div = document.createElement('div');
   div.className = 'todo';
   div.textContent = text;
   div.onclick = function() {
-    if (confirm('Remove this TO DO?')) {
+    if (confirm('Remove this TO DO?')) { /* Pergunta ao usuário se deseja remover o TO DO */
       div.remove();
       saveTodos();
     }
   };
-  var list = document.getElementById('ft_list');
+  var list = document.getElementById('ft_list'); /* Obtém a lista de TO DOs */
   list.insertBefore(div, list.firstChild);
   if (save) saveTodos();
 }
-document.getElementById('new-btn').onclick = function() {
+document.getElementById('new-btn').onclick = function() { /* Define o evento de clique do botão para adicionar um novo TO DO */
   var txt = prompt('Enter a new TO DO:');
   if (txt && txt.trim() !== '') {
     addTodo(txt.trim());
